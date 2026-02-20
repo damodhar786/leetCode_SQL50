e99f82f08e8ad5e712b23d55782f505bae93bb48
Damodhar Vanjarapu <76522052+damodhar786@users.noreply.github.com>
2026-02-20 20:33:16 +0530

    Time: 980 ms (83.19%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,8 @@
 /* Write your T-SQL query statement below */
-SELECT euni.unique_id, emp.name
-FROM Employees emp
-LEFT JOIN EmployeeUNI euni
-ON emp.id = euni.id
+SELECT
+    euni.unique_id unique_id,
+    emp.name name
+FROM EmployeeUNI euni
+RIGHT JOIN Employees emp
+ON euni.id = emp.id
+ORDER BY emp.name ASC
