ab4d5cb05f2bd454284577b8d1e526615de08e7d
Damodhar Vanjarapu <76522052+damodhar786@users.noreply.github.com>
2026-03-22 07:55:11 +0530

    Time: 890 ms (61.60%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,10 +1,14 @@
 /* Write your T-SQL query statement below */
-SELECT
-    activity_date as day,
-    COUNT(DISTINCT(user_id)) as active_users
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
 FROM Activity
-WHERE
-    activity_date BETWEEN '2019-06-28' AND '2019-07-27'
-    AND
-    activity_type IN('open_session', 'end_session', 'scroll_down', 'send_message')
-GROUP BY activity_date
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+  AND CASE 
+        WHEN activity_type = 'open_session' THEN 1
+        WHEN activity_type = 'end_session' THEN 1
+        WHEN activity_type = 'scroll_down' THEN 1
+        WHEN activity_type = 'send_message' THEN 1
+        ELSE 0
+      END = 1
+GROUP BY activity_date;
